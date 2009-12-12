93bf86fb21c6911eb6fdaa795ad5b63c1b270b6b
Al Newkirk <awncorp@cpan.org>
2009-12-12 14:58:31 +0000

    Import of AWNCORP/SweetPea-Cli-0.06 from CPAN.
    gitpan-cpan-distribution: SweetPea-Cli
    gitpan-cpan-version:      0.06
    gitpan-cpan-path:         AWNCORP/SweetPea-Cli-0.06.tar.gz
    gitpan-cpan-author:       AWNCORP
    gitpan-cpan-maturity:     released

--- lib/SweetPea/Cli.pm
+++ lib/SweetPea/Cli.pm
@@ -25,11 +25,11 @@ SweetPea::Cli - Rapid Application Development for SweetPea Packages
 
 =head1 VERSION
 
-Version 0.04
+Version 0.06
 
 =cut
 
-our $VERSION = '0.04';
+our $VERSION = '0.06';
 
 =head1 SYNOPSIS
 
